3c031b23d7d04196cc4039ac6cfa3338d7b76cf7
Zachariah Chow <zachariahchow@gmail.com>
2020-11-03 23:24:22 +0800

    feat: Create Register resolver file

---
+++ src/modules/user/Register.ts
@@ -0,0 +1,31 @@
+import { Query, Mutation, Resolver, Arg } from 'type-graphql';
+import * as bcrypt from 'bcryptjs';
+
+import { User } from '../../entity/User';
+
+@Resolver()
+export class RegisterResolver {
+    @Query(() => String)
+    testQuery(): string {
+        return 'Test Query';
+    }
+
+    @Mutation(() => String)
+    async register(
+        @Arg('firstName') firstName: string,
+        @Arg('lastName') lastName: string,
+        @Arg('email') email: string,
+        @Arg('password') password: string,
+    ): Promise<User> {
+        const hashedPassword = await bcrypt.hash(password, 12);
+
+        const user = await User.create({
+            firstName,
+            lastName,
+            email,
+            password: hashedPassword,
+        }).save();
+
+        return user;
+    }
+}
