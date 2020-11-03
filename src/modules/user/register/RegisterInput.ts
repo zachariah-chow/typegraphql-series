50568b91a7610f457448b04974aea057abe8bb14
Zachariah Chow <zachariahchow@gmail.com>
2020-11-04 00:50:57 +0800

    feat: Add user email input validator

--- src/modules/user/register/RegisterInput.ts
+++ src/modules/user/register/RegisterInput.ts
@@ -1,5 +1,6 @@
 import { Field, InputType } from 'type-graphql';
 import { IsEmail, Length } from 'class-validator';
+import { IsEmailAlreadyExist } from './isEmailAlreadyExist';
 
 @InputType()
 export class RegisterInput {
@@ -13,6 +14,7 @@ export class RegisterInput {
 
     @Field()
     @IsEmail()
+    @IsEmailAlreadyExist({ message: 'email already in use' })
     email: string;
 
     @Field()
